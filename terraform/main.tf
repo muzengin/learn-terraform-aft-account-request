b755d2f69268437dfba5157a654f783f003c5170
mzengin2 <mustafa.zengin@enercity.de>
2024-10-22 14:05:05 +0200

    test aft

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "aws.netz.sandbox@enercity-netz.de"
+    AccountName               = "DevopsSandbox"
+    ManagedOrganizationalUnit = "CentralInfrastructure"
+    SSOUserEmail              = "awsusr9287@enercity-netz.de"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
